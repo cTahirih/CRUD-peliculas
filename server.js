038afbdd97faf7dd1649297a5d6b8d5f81e598cd
Tahirih <tahysluana@gmail.com>
2020-04-26 09:41:59 -0500

    Fix errors

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/code-tidbits'));
+app.use(express.static(__dirname + '/dist/crud-angular'));
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname+ '/dist/code-tidbits/index.html'));
+  res.sendFile(path.join(__dirname+ '/dist/crud-angular/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
